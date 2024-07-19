accdddd58d4b3618c51fcfdaa0a7e041898fe321
Tyler Schmidt <tmschmid@protonmail.com>
2024-07-19 14:58:34 -0700

    Revise Chatting scenario wording. Add more test methods mapping to Chatting scenario.

--- tests/chatting.rs
+++ tests/chatting.rs
@@ -5,7 +5,12 @@ use crate::mock_plugins::{GameWorld, MockStreamerPlugin, MockTiledMapPlugin};
 use bevy::prelude::*;
 use cucumber::{given, then, when, World};
 
+use task_masker::ui::chatting::Msg;
+use task_masker::ui::plugins::ChattingPlugin;
+
 use task_masker::entities::streamer::*;
+use task_masker::entities::MovementType;
+
 use task_masker::map::plugins::TilePosEvent;
 
 #[given("a Streamer is spawned on the map,")]
@@ -23,6 +28,29 @@ fn spawn_streamer(world: &mut GameWorld) {
     world.app.add_event::<TilePosEvent>();
     world.app.add_plugins(MockStreamerPlugin);
     world.app.update();
+}
+
+#[given("the Chatting interface exists,")]
+fn spawn_chatting_ui(world: &mut GameWorld) {
+    world.app.add_plugins(ChattingPlugin);
+    world.app.update();
+}
+
+#[when("the Streamer sends a chat message,")]
+fn streamer_sends_msg(world: &mut GameWorld) {
+    let streamer_msg = Msg {
+        speaker_name: String::from("Caveman"),
+        msg: String::from("This is a test message to see if this works and types as expected."),
+        speaker_role: MovementType::Walk,
+    };
+
+    world.app.world.send_event::<Msg>(streamer_msg);
+    world.app.update();
+}
+
+#[then("the Chatting Queue should contain the Streamer's chat message.")]
+fn chatting_queue_has_streamer_msg(world: &mut GameWorld) {
+    // TODO: Create component called MessageQueue.
 }
 
 fn main() {
--- tests/feature-files/chatting.feature
+++ tests/feature-files/chatting.feature
@@ -1,6 +1,6 @@
 Feature: Chat Messages mapped for Streamer, Subscriber, and Chatter.
     Scenario: The Streamer posts a chat message.
         Given a Streamer is spawned on the map,
-        And the Chatting interface is set up,
+        And the Chatting interface exists,
         When the Streamer sends a chat message,
         Then the Chatting Queue should contain the Streamer's chat message.
