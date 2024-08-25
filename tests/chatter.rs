c3594ab70398aa599536ff94fae50ba3e68989d2
Tyler Schmidt <tmschmid@protonmail.com>
2024-08-25 11:18:41 -0700

    Refactor Chatter tests to use GameWorld's find, find_with, and update methods.

--- tests/chatter.rs
+++ tests/chatter.rs
@@ -30,32 +30,32 @@ fn distance_of(source_pos: TilePos, target_pos: TilePos) -> usize {
 #[given("a Tiled Map")]
 fn spawn_tiled_map(world: &mut GameWorld) {
     world.app.add_plugins(MockTiledMapPlugin);
-    world.app.update();
+    world.update(1);
 
     world.app.add_plugins(PathFindingPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[given("a Chatter spawned on the Tiled Map")]
 fn spawn_chatter_from_tiled_map(world: &mut GameWorld) {
     world.app.add_plugins(MockChatterPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[given("a Streamer spawned on the Tiled Map")]
 fn spawn_streamer_from_tiled_map(world: &mut GameWorld) {
     world.app.add_plugins(MockStreamerPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[when("the Chatter wants to speak")]
 fn make_chatter_approach_to_speak(world: &mut GameWorld) {
-    world.app.world_mut().send_event(ChatMsg {
+    world.broadcast_event(ChatMsg {
         name: String::from("Chatter"),
         msg: String::from("Hello Caveman!"),
     });
 
-    world.app.update();
+    world.update(1);
 }
 
 #[when("the Chatter has approached the Streamer")]
@@ -63,13 +63,10 @@ fn wait_for_chatter_to_approach_to_speak(world: &mut GameWorld) {
     make_chatter_approach_to_speak(world);
 
     loop {
-        world.app.update();
+        world.update(1);
 
         let chatter_status = world
-            .app
-            .world_mut()
-            .query::<&ChatterStatus>()
-            .get_single(&world.app.world())
+            .find::<ChatterStatus>()
             .expect("wait_for_chatter_to_approach_to_speak: Chatter does not have a Status.");
 
         if *chatter_status == ChatterStatus::Speaking {
@@ -83,13 +80,10 @@ fn wait_for_chatter_to_finish_speaking(world: &mut GameWorld) {
     world.app.add_systems(Update, reduce_wait_times_to_zero);
 
     loop {
-        world.app.update();
+        world.update(1);
 
         let chatter_status = world
-            .app
-            .world_mut()
-            .query::<&ChatterStatus>()
-            .get_single(&world.app.world())
+            .find::<ChatterStatus>()
             .expect("wait_for_chatter_to_finish_speaking: Chatter does not have a Status.");
 
         if *chatter_status != ChatterStatus::Speaking {
@@ -100,22 +94,16 @@ fn wait_for_chatter_to_finish_speaking(world: &mut GameWorld) {
 
 #[then("the Chatter will approach the Streamer")]
 fn chatter_should_approach_to_streamer(world: &mut GameWorld) {
-    world.app.update();
+    world.update(1);
 
     let chatter_status = world
-        .app
-        .world_mut()
-        .query::<&ChatterStatus>()
-        .get_single(&world.app.world())
+        .find::<ChatterStatus>()
         .expect("chatter_should_approach_to_streamer: Chatter does not have a Status.");
 
     assert_eq!(*chatter_status, ChatterStatus::Approaching);
 
     let chatter_path = world
-        .app
-        .world_mut()
-        .query_filtered::<&Path, With<ChatterStatus>>()
-        .get_single(&world.app.world())
+        .find_with::<Path, ChatterStatus>()
         .expect("chatter_should_approach_to_streamer: Chatter does not have a Path.");
 
     assert_ne!(chatter_path.len(), 0);
@@ -123,32 +111,21 @@ fn chatter_should_approach_to_streamer(world: &mut GameWorld) {
 
 #[then("the Chatter will be two tiles away from the Streamer")]
 fn chatter_should_be_two_tiles_away_from_streamer(world: &mut GameWorld) {
-    world.app.update();
+    world.update(1);
 
     let chatter_status = world
-        .app
-        .world_mut()
-        .query::<&ChatterStatus>()
-        .get_single(&world.app.world())
+        .find::<ChatterStatus>()
         .expect("chatter_should_approach_to_streamer: Chatter does not have a Status.");
 
     assert_eq!(*chatter_status, ChatterStatus::Speaking);
 
     let chatter_tilepos = *world
-        .app
-        .world_mut()
-        .query_filtered::<&TilePos, With<ChatterLabel>>()
-        .get_single(&world.app.world())
+        .find_with::<TilePos, ChatterLabel>()
         .expect("chatter_should_be_two_tiles_away_from_streamer: Chatter does not have a TilePos.");
 
-    let streamer_tilepos = *world
-        .app
-        .world_mut()
-        .query_filtered::<&TilePos, With<StreamerLabel>>()
-        .get_single(&world.app.world())
-        .expect(
-            "chatter_should_be_two_tiles_away_from_streamer: Streamer does not have a TilePos.",
-        );
+    let streamer_tilepos = *world.find_with::<TilePos, StreamerLabel>().expect(
+        "chatter_should_be_two_tiles_away_from_streamer: Streamer does not have a TilePos.",
+    );
 
     let tile_distance = distance_of(chatter_tilepos, streamer_tilepos);
     assert_eq!(tile_distance, 2);
@@ -156,13 +133,10 @@ fn chatter_should_be_two_tiles_away_from_streamer(world: &mut GameWorld) {
 
 #[then("the Chatter will begin to speak")]
 fn chatter_should_start_speaking(world: &mut GameWorld) {
-    world.app.update();
+    world.update(1);
 
     let chatter_status = world
-        .app
-        .world_mut()
-        .query::<&ChatterStatus>()
-        .get_single(&world.app.world())
+        .find::<ChatterStatus>()
         .expect("chatter_should_start_speaking: Chatter does not have a Status.");
 
     assert_eq!(*chatter_status, ChatterStatus::Speaking);
@@ -171,13 +145,10 @@ fn chatter_should_start_speaking(world: &mut GameWorld) {
 #[then("the Chatter leaves back to its resting point")]
 fn chatter_should_be_leaving_back_to_spawn(world: &mut GameWorld) {
     loop {
-        world.app.update();
+        world.update(1);
 
         let chatter_status = world
-            .app
-            .world_mut()
-            .query::<&ChatterStatus>()
-            .get_single(&world.app.world())
+            .find::<ChatterStatus>()
             .expect("chatter_should_be_leaving_back_to_spawn: Chatter does not have a Status.");
 
         if *chatter_status == ChatterStatus::Idle {
