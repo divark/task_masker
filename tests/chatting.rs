4e7147ff5a953d5e76bc115d8bc2ed33f7f343b4
Tyler Schmidt <tmschmid@protonmail.com>
2024-09-03 16:16:50 -0700

    Add test confirming Chatter leaving prematurely when speaking.

--- tests/chatting.rs
+++ tests/chatting.rs
@@ -9,6 +9,7 @@ use mock_plugins::{
 use bevy::prelude::*;
 use cucumber::{given, then, when, World};
 
+use task_masker::entities::chatter::ChatterStatus;
 use task_masker::entities::GameEntityType;
 use task_masker::map::plugins::TilePosEvent;
 use task_masker::ui::chatting::*;
@@ -65,6 +66,30 @@ impl GameWithChatUI {
             .get_single(&self.app.world())
             .ok()
     }
+
+    /// Returns the requested Component found associated
+    /// with the other Component within the game, or None
+    /// otherwise.
+    pub fn find_with<T, U>(&mut self) -> Option<&T>
+    where
+        T: Component,
+        U: Component,
+    {
+        self.app
+            .world_mut()
+            .query_filtered::<&T, With<U>>()
+            .get_single(&self.app.world())
+            .ok()
+    }
+
+    /// Sends an Event to all systems listening to it
+    /// in the game.
+    pub fn broadcast_event<T>(&mut self, event: T)
+    where
+        T: Event,
+    {
+        self.app.world_mut().send_event(event);
+    }
 }
 
 /// Sets each Typing Timer to zero to make testing
@@ -113,25 +138,25 @@ fn spawn_streamer(world: &mut GameWithChatUI) {
     // include it here, despite it not being used
     // under these tests.
     world.app.add_plugins(MockStreamerPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[given("a Chatter is spawned on the map,")]
 fn spawn_chatter(world: &mut GameWithChatUI) {
     world.app.add_plugins(MockChatterPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[given("a Subscriber is spawned on the map,")]
 fn spawn_subscriber(world: &mut GameWithChatUI) {
     world.app.add_plugins(MockSubscriberPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[given("the Chatting interface exists,")]
 fn spawn_chatting_ui(world: &mut GameWithChatUI) {
     world.app.add_plugins(MockChattingPlugin);
-    world.app.update();
+    world.update(1);
 }
 
 #[when("the Streamer sends a chat message,")]
@@ -142,12 +167,8 @@ fn streamer_sends_msg(world: &mut GameWithChatUI) {
         GameEntityType::Walk,
     );
 
-    world
-        .app
-        .world_mut()
-        .send_event::<Msg>(streamer_msg.clone());
-    world.app.update();
-    world.app.update();
+    world.broadcast_event::<Msg>(streamer_msg.clone());
+    world.update(2);
 
     world.sent_msgs.push(streamer_msg);
 }
@@ -160,9 +181,20 @@ fn chatter_sends_msg(world: &mut GameWithChatUI) {
         GameEntityType::Fly,
     );
 
-    world.app.world_mut().send_event::<Msg>(chatter_msg.clone());
-    world.app.update();
-    world.app.update();
+    world.broadcast_event::<Msg>(chatter_msg.clone());
+    world.update(2);
+
+    world.sent_msgs.push(chatter_msg);
+}
+
+#[when("the Chatter sends a long chat message,")]
+fn chatter_sends_long_msg(world: &mut GameWithChatUI) {
+    let long_msg = String::from("So, if you're learning a subject of math for the first time, it's helpful to actually learn about the concepts behind it before going into the course, since you're otherwise being overloaded with a bunch of terminology. Doing it this way, it's important to do so with the angle of finding how it's important to your work, using analogies and metaphors to make the knowledge personal");
+
+    let chatter_msg = Msg::new(String::from("Chatter"), long_msg, GameEntityType::Fly);
+
+    world.broadcast_event::<Msg>(chatter_msg.clone());
+    world.update(2);
 
     world.sent_msgs.push(chatter_msg);
 }
@@ -175,12 +207,8 @@ fn subscriber_sends_msg(world: &mut GameWithChatUI) {
         GameEntityType::Swim,
     );
 
-    world
-        .app
-        .world_mut()
-        .send_event::<Msg>(subscriber_msg.clone());
-    world.app.update();
-    world.app.update();
+    world.broadcast_event::<Msg>(subscriber_msg.clone());
+    world.update(2);
 
     world.sent_msgs.push(subscriber_msg);
 }
@@ -226,51 +254,99 @@ fn wait_until_wait_time_is_up(world: &mut GameWithChatUI) {
     }
 }
 
+#[when("the Chatter is almost done speaking to the Streamer,")]
+fn wait_until_chatter_near_end_of_speaking(world: &mut GameWithChatUI) {
+    loop {
+        world.update(1);
+
+        let msg_index = world
+            .find::<TypingMsg>()
+            .expect(
+                "wait_until_chatter_near_end_of_speaking: Could not find Typing Indicator type.",
+            )
+            .idx();
+        if msg_index > 30 {
+            break;
+        }
+    }
+}
+
+#[then("the Chatter should still be speaking.")]
+fn chatter_should_still_be_speaking(world: &mut GameWithChatUI) {
+    world.update(1);
+
+    let msg_is_still_being_typed = !world
+        .find::<TypingMsg>()
+        .expect("chatter_should_still_be_speaking: Typing Indicator could not be found")
+        .at_end();
+    assert!(msg_is_still_being_typed);
+
+    let expected_chatter_status = ChatterStatus::Speaking;
+    let actual_chatter_status = world
+        .find::<ChatterStatus>()
+        .expect("chatter_should_still_be_speaking: Chatter status could not be found.");
+
+    assert_eq!(expected_chatter_status, *actual_chatter_status);
+}
+
 #[then(
     regex = r"^the Chatting Queue should contain the (Streamer|Chatter|Subscriber)'s chat message."
 )]
 fn chatting_queue_has_streamer_msg(world: &mut GameWithChatUI) {
-    world.app.update();
+    world.update(1);
+
+    let expected_msg_contents = world.sent_msgs.get(0).unwrap().clone();
 
     let pending_chat_messages = world
-        .app
-        .world_mut()
-        .query::<&MessageQueue>()
-        .single(&world.app.world());
-
+        .find::<MessageQueue>()
+        .expect("chatting_queue_has_streamer_msg: Could not find Message Queue.");
     let next_chat_msg = pending_chat_messages.peek();
     assert!(next_chat_msg.is_some());
 
-    let next_chat_msg_contents = next_chat_msg.unwrap();
-    assert_eq!(*world.sent_msgs.get(0).unwrap(), *next_chat_msg_contents);
+    let actual_chat_msg_contents = next_chat_msg.unwrap();
+    assert_eq!(expected_msg_contents, *actual_chat_msg_contents);
 }
 
 #[then("the Chatting Queue should have the Streamer's chat message as the top priority.")]
 fn chatting_queue_has_streamer_msg_top_priority(world: &mut GameWithChatUI) {
-    world.app.update();
+    world.update(1);
+
+    // For testing purposes, the Streamer's message is the second one we've recorded
+    // from earlier steps defined in this file.
+    let expected_msg_contents = world.sent_msgs.get(1).unwrap().clone();
 
     let pending_chat_messages = world
-        .app
-        .world_mut()
-        .query::<&MessageQueue>()
-        .single(&world.app.world());
-
+        .find::<MessageQueue>()
+        .expect("chatting_queue_has_streamer_msg: Could not find Message Queue.");
     let next_chat_msg = pending_chat_messages.peek();
     assert!(next_chat_msg.is_some());
 
-    let next_chat_msg_contents = next_chat_msg.unwrap();
-    assert_eq!(*world.sent_msgs.get(1).unwrap(), *next_chat_msg_contents);
+    let actual_chat_msg_contents = next_chat_msg.unwrap();
+    assert_eq!(expected_msg_contents, *actual_chat_msg_contents);
+}
+
+#[then(regex = r"there should be ([0-9]+) messages? in the Message Queue,")]
+fn chatting_queue_has_n_messages(world: &mut GameWithChatUI, num_messages: String) {
+    world.update(1);
+
+    let chatting_queue = world
+        .find::<MessageQueue>()
+        .expect("chatting_queue_has_n_messages: Message Queue was not found.");
+
+    let expected_num_messages = num_messages
+        .parse::<usize>()
+        .expect("chatting_queue_has_n_messages: Cannot convert number from then step.");
+    let actual_num_messages = chatting_queue.len();
+    assert_eq!(expected_num_messages, actual_num_messages);
 }
 
 #[then("the Chat UI should contain the first five characters typed from the Chat Message.")]
 fn chatting_ui_contains_first_five_chars_from_msg(world: &mut GameWithChatUI) {
     world.update(1);
 
-    let msg_txtfield = world
-        .app
-        .world_mut()
-        .query_filtered::<&Text, With<SpeakerChatBox>>()
-        .single(&world.app.world());
+    let msg_txtfield = world.find_with::<Text, SpeakerChatBox>().expect(
+        "chatting_ui_contains_first_five_chars_from_msg: Could not find Text from SpeakerChatBox.",
+    );
 
     let expected_contents = String::from("👍This");
     let msg_contents = read_first_n(&msg_txtfield, 5);
@@ -291,10 +367,7 @@ fn chat_ui_should_be_hidden(world: &mut GameWithChatUI) {
     world.update(1);
 
     let ui_visibility = world
-        .app
-        .world_mut()
-        .query_filtered::<&Visibility, With<SpeakerUI>>()
-        .get_single(&world.app.world())
+        .find_with::<Visibility, SpeakerUI>()
         .expect("chat_ui_should_be_hidden: Could not find Visibility for Chat UI.");
 
     assert_eq!(Visibility::Hidden, *ui_visibility);
--- tests/feature-files/chatting.feature
+++ tests/feature-files/chatting.feature
@@ -3,27 +3,31 @@ Feature: Chat Messages mapped for Streamer, Subscriber, and Chatter.
         Given a Streamer is spawned on the map,
         And the Chatting interface exists,
         When the Streamer sends a chat message,
+        Then there should be 1 messages in the Message Queue,
         Then the Chatting Queue should contain the Streamer's chat message.
 
     Scenario: The Chatter posts a chat message.
-    	Given a Chatter is spawned on the map,
-    	And the Chatting interface exists,
-    	When the Chatter sends a chat message,
-    	Then the Chatting Queue should contain the Chatter's chat message.
+        Given a Chatter is spawned on the map,
+        And the Chatting interface exists,
+        When the Chatter sends a chat message,
+        Then there should be 1 message in the Message Queue,
+        Then the Chatting Queue should contain the Chatter's chat message.
 
     Scenario: The Subscriber posts a chat message.
-    	Given a Subscriber is spawned on the map,
-    	And the Chatting interface exists,
-    	When the Subscriber sends a chat message,
-    	Then the Chatting Queue should contain the Subscriber's chat message.
+        Given a Subscriber is spawned on the map,
+        And the Chatting interface exists,
+        When the Subscriber sends a chat message,
+        Then there should be 1 message in the Message Queue,
+        Then the Chatting Queue should contain the Subscriber's chat message.
 
     Scenario: The Chatter posts a chat message, and the Streamer does at the same time.
-    	Given a Streamer is spawned on the map,
-    	And a Chatter is spawned on the map,
-    	And the Chatting interface exists,
-    	When the Chatter sends a chat message,
-    	And the Streamer sends a chat message,
-    	Then the Chatting Queue should have the Streamer's chat message as the top priority.
+        Given a Streamer is spawned on the map,
+        And a Chatter is spawned on the map,
+        And the Chatting interface exists,
+        When the Chatter sends a chat message,
+        And the Streamer sends a chat message,
+        Then there should be 2 messages in the Message Queue,
+        And the Chatting Queue should have the Streamer's chat message as the top priority.
 
     Scenario: A Chat Message is read one character at a time.
         Given a Streamer is spawned on the map,
@@ -51,3 +55,11 @@ Feature: Chat Messages mapped for Streamer, Subscriber, and Chatter.
         And the Chatter has finished speaking to the Streamer the first time,
         Then the Chatter should not be leaving,
         And the Chatter should start speaking from the next chat message,
+
+    Scenario: A Chatter should not leave until done speaking.
+        Given a Streamer is spawned on the map,
+        And a Chatter is spawned on the map,
+        And the Chatting interface exists,
+        When the Chatter sends a long chat message,
+        And the Chatter is almost done speaking to the Streamer,
+        Then the Chatter should still be speaking.
